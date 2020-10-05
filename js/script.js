c5004a20edc444ddcde10cf8974f16183e080975
beulasamuel <beaulathamban@gmail.com>
2020-10-06 11:57:47 +1300

    icon, navbar

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// document.getElementById('home').addEventListener('click', function(){
+//   alert('home');
+// });
+
+$(document).ready(function(){
+  // $('body').click(function(){
+  //   alert('clicked');
+  // });
+
+
+});
